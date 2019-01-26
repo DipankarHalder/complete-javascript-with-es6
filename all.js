de916bede22667a8618b886cf3ab5475ca5ee879
dipankar <webmail.dip@gmail.com>
2019-01-26 20:24:00 +0530

    n ew files init

---
+++ all.js
@@ -0,0 +1,102 @@
+/*
+    *****************************************************
+    $  use strict concept
+    ***************************************************** 
+
+        -   The directive looks like a string: "use strict" or 'use strict'. When it is located 
+            at the top of a script, the whole script works the “modern” way.
+
+        -   It is not a statement, but a literal expression, ignored by earlier versions of JavaScript.
+            Strict mode is enabled by placing "use strict" at the top of a script or function. 
+            Several language features, like “classes” and “modules”, enable strict mode automatically.
+
+        -   Strict mode is supported by all modern browsers.
+
+        -   How to use :  "use strict";
+
+
+
+
+
+
+    *****************************************************
+    $  Declearation & Defination
+    ***************************************************** 
+
+    1.  Declearation  -  When ask the complier or interprater to create a new veriable and give it to a name.
+    2.  Defination    -  When you assign a value in the decleared veriable.
+
+    Example  -  
+        var a;                  // called Declearation
+        a = "Hello World !"     // called Defination
+
+
+
+    *****************************************************
+    $  Undefined & null
+    *****************************************************
+
+
+
+    *****************************************************
+    $  Scope concept
+    *****************************************************
+
+
+
+    *****************************************************
+    $  Var, Let & Const
+    *****************************************************
+
+
+
+    *****************************************************
+    $  Opreator (=, ==, ==, **)
+    *****************************************************
+
+
+
+
+    *****************************************************
+    $  Opreator (+=, -=, ++, --)
+    *****************************************************
+
+
+
+
+    *****************************************************
+    $  Object Concept
+    *****************************************************
+    
+
+
+
+
+    *****************************************************
+    $  Function Concept
+    *****************************************************
+
+
+
+
+    *****************************************************
+    $  Constructor Concept
+    *****************************************************
+
+
+
+
+    *****************************************************
+    $  Array Concept
+    *****************************************************
+
+
+
+
+    *****************************************************
+    $  It
+    *****************************************************
+
+
+
+    */
