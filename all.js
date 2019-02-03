25733eef0c50055ae6d0da09a1d6e94edc6500a6
dipankar <webmail.dip@gmail.com>
2019-02-03 19:48:18 +0530

    modified all files

--- all.js
+++ all.js
@@ -29,9 +29,40 @@
 
 
 
+
+    *****************************************************
+    $  Unary, Binary, Operand
+    *****************************************************
+        1. Operand - An operand is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
+        2. Unary - An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
+            Example:    let x = 1;
+            --------    x = -x;
+                        alert( x ); // -1, unary negation was applied 
+
+        3. Binary - An operator is binary if it has two operands. The same minus exists in binary form as well:
+            Example:    let x = 1, y = 3;
+            --------    alert( y - x ); // 2, binary minus subtracts values
+
+    
+
+
+    *****************************************************
+    $  Opreator (=, ==, ==, **)
+    *****************************************************
+        1. =    :  Assignment Operator
+        2. ==   :  Comparisons - Formerly Compares Variable.
+        3. ===  :  Strict equality - Checks the equality with type of value
+        4. **   :  Exponentiation Operator, It's a recent addition to the language. 
+                   For a natural number b, the result of a ** b is a multiplied by itself b times.
+
+
+
+    
     *****************************************************
     $  Undefined & null
     *****************************************************
+
+
 
 
 
@@ -40,28 +71,6 @@
     *****************************************************
 
 
-
-    *****************************************************
-    $  Var, Let & Const
-    *****************************************************
-
-
-
-    *****************************************************
-    $  Opreator (=, ==, ==, **)
-    *****************************************************
-
-
-
-
-    *****************************************************
-    $  Opreator (+=, -=, ++, --)
-    *****************************************************
-
-
-
-
-    
 
 
     *****************************************************
